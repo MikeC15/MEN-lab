80ff1257575c61b765949573e557be95a9ed975f
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-10-18 14:11:11 -0600

    new page route added

---
+++ controllers/actors.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const router = express.Router();
+
+// const Actor = require("../models/actors")
+
+
+
+// /controllers
+
+// new route
+router.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// create route
+
+// edit route
+
+// update route 
+
+// show route
+
+// index route
+
+// destroy route
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const actorsController = require("./controllers/actors")
+
+app.use("/actors", actorsController)
+
 app.listen(3000, () => {
     console.log('We are LisTeNinG');
 });
